e7caf9c7af9ee053517518ff291056bf39271e25
Joakim Hellstedt <joakim.v.hellstedt@gmail.com>
2024-03-02 21:24:54 +0100

    Created sidenav and topnav
    -Updated styleformat to scss
    -Added font

--- RecipeApplication.Client/src/app/app.module.ts
+++ RecipeApplication.Client/src/app/app.module.ts
@@ -4,16 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
