6e07bfb21a82d70d138141dd4689bbb5b5ef802e
Joakim Hellstedt <joakim.v.hellstedt@gmail.com>
2024-03-12 22:29:31 +0100

    Added Identity with JWT

---
+++ RecipeApplication.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RecipeApplication.WebAPI.Models;
+
+namespace RecipeApplication.WebAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : 
+            base(dbContextOptions) 
+        {
+
+        }
+    }
+}
---
+++ RecipeApplication.WebAPI/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RecipeApplication.WebAPI.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+    }
+}
