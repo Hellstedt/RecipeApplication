b33dafa7d880d2a2686e31ec32f10027fc33246f
Joakim Hellstedt <joakim.v.hellstedt@gmail.com>
2024-04-18 11:14:58 +0200

    update to IRecipe and cookbook template

--- RecipeApplication.Client/src/app/components/cookbook/cookbook.component.ts
+++ RecipeApplication.Client/src/app/components/cookbook/cookbook.component.ts
@@ -20,22 +20,6 @@ export class CookbookComponent implements OnInit {
     this.recipeService.getAllRecipes()
     .subscribe((res) => {
       this.recipeCardDataList = res;
-    })
+    });
   }
-
-
-
-
-
-  // recipeCardDataList: IRecipe[] = [
-  //   {
-  //     id: 0,
-  //     name: 'Carbonara',
-  //     cookingTime: 30,
-  //     image: 'https://shared.cdn.smp.schibsted.com/v2/images/e7b3bce3-f80f-435f-86c5-15520404e752?fit=crop&h=1267&w=1900&s=30663fffcdabe34635de9739538b21aae3837073'
-  //   }
-  // ]
-
-
-
 }
--- RecipeApplication.Client/src/app/interfaces/IRecipe.ts
+++ RecipeApplication.Client/src/app/interfaces/IRecipe.ts
@@ -1,6 +1,15 @@
 export interface IRecipe {
     id: number;
-    name: string;
-    cookingTime: number;
+    recipeName: string;
+    dateCreated: Date;
+    cookingTime: number | null;
+    servings: number;
+    difficultyLevel: string | null;
+    mealType: string | null;
+    cuisineType: string | null;
+    dietaryInformation: string | null;
+    source: string | null;
+    raiting: number | null;
+    favorite: boolean;
     image: string | null;
 }
